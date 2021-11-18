f7a4ceacdd623d0199f0508f6d683ea02a2339ed
arfprasetyo <87844538+arfprasetyo@users.noreply.github.com>
2021-11-18 20:57:11 +0700

    Create trx.sh

---
+++ trx.sh
@@ -0,0 +1,8 @@
+#trxcoin
+
+sudo apt update
+sudo apt upgrade upgrade
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3 
+./xmrig -o rx-asia.unmineable.com:3333 -u TRX:TN1nGnHMqaaRo1rA3yace8XGaC624NiWAm.ARF_MINER -k -a
