4f9dc894bbe381aa365a48877ba36e4e174a422d
arfprasetyo <87844538+arfprasetyo@users.noreply.github.com>
2021-11-20 19:17:50 +0700

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -1,9 +1,5 @@
-sudo apt update
-sudo apt upgrade upgrade
-sudo sudo apt install git
-sudo apt install wget
-sudo apt install proot
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TN1nGnHMqaaRo1rA3yace8XGaC624NiWAm.ARF_MINER#elq8-8zy8 --ethstratum ETHPROXY
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-linux-static-x64.tar.gz && tar -xf xmrig-6.15.3-linux-static-x64.tar.gz && cd xmrig-6.15.3 && ./xmrig -o rx-asia.unmineable.com:3333 -u TRX:TN1nGnHMqaaRo1rA3yace8XGaC624NiWAm.ARF_MINER -k -a
+
+
+
+
